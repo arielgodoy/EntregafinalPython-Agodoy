69d93305c5d6feccbee186a8989a4a1a1b953d54
arielgodoy <arielgodoy@gmasil.com>
2023-08-07 16:28:10 -0400

    todo ok desde otro pc

--- acounts/views.py
+++ acounts/views.py
@@ -8,6 +8,8 @@ from .forms import CustomUserForm
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login,logout
+import time
+
 
 # Create your views here.
 
@@ -24,6 +26,26 @@ def login_view(request):
             error_message = "Nombre de usuario o contraseña incorrectos."
             return render(request, 'login.html', {'error_message': error_message})
     return render(request, 'login.html')
+
+def logout_view(request):
+    # Vista para realizar el logout (opcional)
+    logout(request)
+    return redirect('login')
+
+
+
+def registro_usuario(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Usuario {username} creado con éxito. Por favor, inicia sesión.')
+            time.sleep(5)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registro_usuario.html', {'form': form})
 
 
 
@@ -54,19 +76,3 @@ def cambiar_password(request):
         form = PasswordChangeForm(request.user)
     return render(request, 'cambiar_password.html', {'form': form})
 
-def logout_view(request):
-    # Vista para realizar el logout (opcional)
-    logout(request)
-    return redirect('login')
-
-def registro_usuario(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Usuario {username} creado con éxito. Por favor, inicia sesión.')
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registro_usuario.html', {'form': form})
